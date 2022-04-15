d81fd5d7e48f5481ceee7c669001031c2a1b934a
icoez <78701335+icoez@users.noreply.github.com>
2022-04-15 20:01:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,95 @@
+let obj;
+let question;
+
+fetch("./assets/data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    obj = data;
+    changeQuestion();
+  });
+
+const playQuestion = $("#playQuestion");
+let qPaused = true;
+let qAudio;
+
+const playAnswer = $("#playAnswer");
+let aPaused = true;
+let aAudio;
+
+const shuffle = $(".shuffle");
+
+let myTimeout;
+
+$(playQuestion).on("click", function () {
+  if (qPaused) {
+    if (!aPaused) {
+      $(playAnswer).attr("src", "assets/icons8-play-100.png");
+      aPaused = true;
+      aAudio.pause();
+    }
+    $(playQuestion).attr("src", "assets/icons8-pause-100.png");
+    qPaused = false;
+    qAudio = new Audio(obj[question]["nuria"]);
+    qAudio.play();
+    $(qAudio).on("loadedmetadata", function () {
+      myTimeout = setTimeout(() => {
+        if (!qPaused) {
+          $(playQuestion).attr("src", "assets/icons8-play-100.png");
+          qPaused = true;
+        }
+      }, qAudio.duration * 1000);
+    });
+  } else {
+    $(playQuestion).attr("src", "assets/icons8-play-100.png");
+    qPaused = true;
+    qAudio.pause();
+  }
+});
+$(playAnswer).on("click", function () {
+  if (aPaused) {
+    if (!qPaused) {
+      $(playQuestion).attr("src", "assets/icons8-play-100.png");
+      qPaused = true;
+      qAudio.pause();
+    }
+    $(playAnswer).attr("src", "assets/icons8-pause-100.png");
+    aPaused = false;
+    aAudio = new Audio(obj[question]["felix"]);
+    aAudio.play();
+    $(aAudio).on("loadedmetadata", function () {
+      myTimeout = setTimeout(() => {
+        if (!aPaused) {
+          $(playAnswer).attr("src", "assets/icons8-play-100.png");
+          aPaused = true;
+        }
+      }, aAudio.duration * 1000);
+    });
+  } else {
+    $(playAnswer).attr("src", "assets/icons8-play-100.png");
+    aPaused = true;
+    aAudio.pause();
+  }
+});
+$(shuffle).on("click", () => {
+  if (!aPaused) {
+    aAudio.pause();
+    $(playAnswer).attr("src", "assets/icons8-play-100.png");
+    aPaused = true;
+  } else if (!qPaused) {
+    qAudio.pause();
+    $(playQuestion).attr("src", "assets/icons8-play-100.png");
+    qPaused = true;
+  }
+  clearTimeout(myTimeout);
+  changeQuestion();
+  $(".hide").each(function () {
+    $(this).removeClass("hide");
+  });
+});
+const playAudio = (src) => {
+  sound = new Audio();
+  sound.play();
+};
+const changeQuestion = () => {
+  question = `${Math.floor(Math.random() * Object.keys(obj).length)}`;
+};
